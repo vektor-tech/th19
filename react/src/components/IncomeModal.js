1ba248473e97b19d8232c83a857735baacd7bec2
Aabhas Kharel <aabhas.kharel@mavs.uta.edu>
2019-01-27 10:34:04 -0600

    added modal

---
+++ react/src/components/IncomeModal.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button, Header, Image, Modal, Icon } from "semantic-ui-react";
+import "../styles/IncomeModal.css";
+
+const ModalModalExample = () => (
+  <div className="income-modal">
+    <Modal
+      trigger={
+        <Button size="small">
+          <Icon name="edit" class="edit-button" size="large" />
+        </Button>
+      }
+      size="tiny"
+    >
+      <Modal.Header>Income</Modal.Header>
+      <Modal.Content>
+        <Modal.Description>
+          <input className="modal-input" type="text" placeholder="Name" />
+          <input className="modal-input" type="number" placeholder="Value" />
+        </Modal.Description>
+        <button type="submit">Done</button>
+      </Modal.Content>
+    </Modal>
+  </div>
+);
+
+export default ModalModalExample;
