6bfbad5b93807d5650406385fac43f36c36ecce5
Aabhas Kharel <aabhas.kharel@mavs.uta.edu>
2019-01-27 21:26:54 -0600

    minor changes

--- react/src/components/NetWorthGraph.js
+++ react/src/components/NetWorthGraph.js
@@ -3,21 +3,29 @@ import { Area, AreaChart, XAxis, YAxis, Tooltip } from "recharts";
 
 const NetWorthGraph = ({ nwArray }) => {
   nwArray = nwArray.map(({ value, timestamp }) => {
-    return { value: value / 100, timestamp: new Date(timestamp).getFullYear() };
+    return {
+      value: value / 100,
+      timestamp: new Date(timestamp).getFullYear()
+    };
   });
   return (
     <AreaChart
       width={750}
       height={250}
       data={nwArray}
-      margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+      margin={{
+        top: 10,
+        right: 30,
+        left: 0,
+        bottom: 0
+      }}
     >
       <defs>
         <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-          <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-          <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-        </linearGradient>
-      </defs>
+          <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />{" "}
+          <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />{" "}
+        </linearGradient>{" "}
+      </defs>{" "}
       <XAxis dataKey="timestamp" />
       <YAxis />
       <Tooltip />
